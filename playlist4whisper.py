2a73fe80da6783179be06b7212116bddcd8cf7c4
Antonio R <33932744+antor44@users.noreply.github.com>
2023-04-21 19:04:40 +0200

    playlist4whisper.py v. 1.24

--- playlist4whisper.py
+++ playlist4whisper.py
@@ -1,9 +1,11 @@
 """
 
-"playlist4whisper" is an application that displays a playlist for livestream_video.sh, a simple GUI using Python and the tkinter library. It plays online videos and transcribes livestreams by feeding the output of ffmpeg to whisper.cpp, based on livestream.sh from whisper.cpp.
+"playlist4whisper" is an application that displays a playlist for livestream_video.sh, a simple GUI using Python and
+the tkinter library. It plays online videos and transcribes livestreams by feeding the output of ffmpeg to
+whisper.cpp, based on livestream.sh from whisper.cpp.
 
 
-Author: Antonio R. Version: 1.22 License: MIT
+Author: Antonio R. Version: 1.24 License: MIT
 
 
 
@@ -14,14 +16,19 @@ python playlist4whisper.py
 The program will load default playlist.m3u, and will store options in config.json
 
 
-This program depends on other Linux programs and their libraries, such as whisper.cpp and mpv. The script livestream_video.sh should be in the same directory as the compiled executable of whisper.cpp, which should have the default name "main". Additionally, it is necessary to download the Whisper model file from OpenAI and place it in the "models" directory with the correct format and name, as specified in the Whisper.cpp repository. This can be done using terminal commands like one of the following examples:
+This program depends on other Linux programs and their libraries, such as whisper.cpp and mpv. The script
+livestream_video.sh should be in the same directory as the compiled executable of whisper.cpp, which should have the
+default name "main". Additionally, it is necessary to download the Whisper model file from OpenAI and place it in the
+"models" directory with the correct format and name, as specified in the Whisper.cpp repository. This can be done
+using terminal commands like one of the following examples:
 
 make tiny.en
 
 make small
 
 
-playlist4whisper.py dependes on (smplayer, mpv or mplayer) video player and (gnome-terminal or konsole or xfce4-terminal).
+playlist4whisper.py depends on (smplayer, mpv or mplayer) video player and (gnome-terminal or konsole or
+xfce4-terminal).
 
 
 Options for script:
@@ -39,7 +46,19 @@ Whisper models: tiny.en, tiny, base.en, base, small.en, small, medium.en, medium
 
 Whisper languages:
 
-Autodetected (auto), English (en), Chinese (zh), German (de), Spanish (es), Russian (ru), Korean (ko), French (fr), Japanese (ja), Portuguese (pt), Catalan (ca), Dutch (nl), Arabic (ar), Italian (it), Hebrew (iw), Ukrainian (uk), Romanian (ro), Persian (fa), Swedish (sv), Indonesian (id), Hindi (hi), Finnish (fi), Vietnamese (vi), Hebrew (iw), Ukrainian (uk), Greek (el), Malay (ms), Czech (cs), Romanian (ro), Danish (da), Hungarian (hu), Tamil (ta), Norwegian (no), Thai (th), Urdu (ur), Croatian (hr), Bulgarian (bg), Lithuanian (lt), Latin (la), Maori (mi), Malayalam (ml), Welsh (cy), Slovak (sk), Telugu (te), Persian (fa), Latvian (lv), Bengali (bn), Serbian (sr), Azerbaijani (az), Slovenian (sl), Kannada (kn), Estonian (et), Macedonian (mk), Breton (br), Basque (eu), Icelandic (is), Armenian (hy), Nepali (ne), Mongolian (mn), Bosnian (bs), Kazakh (kk), Albanian (sq), Swahili (sw), Galician (gl), Marathi (mr), Punjabi (pa), Sinhala (si), Khmer (km), Shona (sn), Yoruba (yo), Somali (so), Afrikaans (af), Occitan (oc), Georgian (ka), Belarusian (be), Tajik (tg), Sindhi (sd), Gujarati (gu), Amharic (am), Yiddish (yi), Lao (lo), Uzbek (uz), Faroese (fo), Haitian Creole (ht), Pashto (ps), Turkmen (tk), Nynorsk (nn), Maltese (mt), Sanskrit (sa), Luxembourgish (lb), Myanmar (my), Tibetan (bo), Tagalog (tl), Malagasy (mg), Assamese (as), Tatar (tt), Hawaiian (haw), Lingala (ln), Hausa (ha), Bashkir (ba), Javanese (jw), Sundanese (su).
+Autodetected (auto), English (en), Chinese (zh), German (de), Spanish (es), Russian (ru), Korean (ko), French (fr),
+Japanese (ja), Portuguese (pt), Catalan (ca), Dutch (nl), Arabic (ar), Italian (it), Hebrew (iw), Ukrainian (uk),
+Romanian (ro), Persian (fa), Swedish (sv), Indonesian (id), Hindi (hi), Finnish (fi), Vietnamese (vi), Hebrew (iw),
+Ukrainian (uk), Greek (el), Malay (ms), Czech (cs), Romanian (ro), Danish (da), Hungarian (hu), Tamil (ta),
+Norwegian (no), Thai (th), Urdu (ur), Croatian (hr), Bulgarian (bg), Lithuanian (lt), Latin (la), Maori (mi),
+Malayalam (ml), Welsh (cy), Slovak (sk), Telugu (te), Persian (fa), Latvian (lv), Bengali (bn), Serbian (sr),
+Azerbaijani (az), Slovenian (sl), Kannada (kn), Estonian (et), Macedonian (mk), Breton (br), Basque (eu), Icelandic (
+is), Armenian (hy), Nepali (ne), Mongolian (mn), Bosnian (bs), Kazakh (kk), Albanian (sq), Swahili (sw),
+Galician (gl), Marathi (mr), Punjabi (pa), Sinhala (si), Khmer (km), Shona (sn), Yoruba (yo), Somali (so),
+Afrikaans (af), Occitan (oc), Georgian (ka), Belarusian (be), Tajik (tg), Sindhi (sd), Gujarati (gu), Amharic (am),
+Yiddish (yi), Lao (lo), Uzbek (uz), Faroese (fo), Haitian Creole (ht), Pashto (ps), Turkmen (tk), Nynorsk (nn),
+Maltese (mt), Sanskrit (sa), Luxembourgish (lb), Myanmar (my), Tibetan (bo), Tagalog (tl), Malagasy (mg),
+Assamese (as), Tatar (tt), Hawaiian (haw), Lingala (ln), Hausa (ha), Bashkir (ba), Javanese (jw), Sundanese (su).
 
 translate: The "translate" option provides automatic English translation (only English is available).
 
@@ -69,9 +88,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-
-
-
 import os
 import re
 import json
@@ -80,7 +96,7 @@ import tkinter as tk
 from tkinter import filedialog, ttk, simpledialog
 
 # Default options
-default_mpv_options = "-geometry 1280"
+default_mpv_options = "-geometry 1100"
 default_bash_options = "4 base auto"
 bash_script = "./livestream_video.sh"
 config_file = "config.json"
@@ -91,20 +107,35 @@ class M3uPlaylistPlayer(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        self.load_button = None
+        self.load_label = None
+        self.save_button = None
+        self.edit_button = None
+        self.delete_button = None
+        self.add_button = None
+        self.add_label = None
+        self.override_checkbox = None
+        self.save_options_id = None
+        self.bash_options_entry = None
+        self.bash_options_label = None
+        self.mpv_options_entry = None
+        self.mpv_options_label = None
+        self.options_frame = None
+        self.tree = None
         self.title("playlist4whisper")
         self.geometry("800x800")
 
         self.playlist = []
         self.current_options = {}
-        self.list_number=0
-        self.load_config()
+        self.list_number = 0
         self.override_options = tk.BooleanVar()
         self.create_widgets()
-        self.populate_playlist()
         self.mpv_fg = self.mpv_options_entry.cget("fg")
         self.mpv_bg = self.mpv_options_entry.cget("bg")
         self.bash_fg = self.bash_options_entry.cget("fg")
         self.bash_bg = self.bash_options_entry.cget("bg")
+        self.load_config()
+        self.populate_playlist()
 
     def create_widgets(self):
         self.tree = ttk.Treeview(self, columns=("list_number", "name", "url"), show="headings")
@@ -147,7 +178,6 @@ class M3uPlaylistPlayer(tk.Tk):
         self.override_checkbox = tk.Checkbutton(self.options_frame, text='Override options', variable=self.override_options, command=self.change_override)
         self.override_checkbox.pack(side=tk.LEFT, padx=10)
 
-
         self.add_label = tk.Label(self, text="Channel:", padx=10)
         self.add_label.pack(side=tk.LEFT)
 
@@ -178,7 +208,6 @@ class M3uPlaylistPlayer(tk.Tk):
         self.load_button = tk.Button(self, text="About", command=self.show_about_window)
         self.load_button.pack(side=tk.LEFT)
 
-
     def populate_playlist(self, filename="playlist.m3u"):
         try:
             with open(filename, "r", encoding='utf-8') as file:
@@ -186,24 +215,21 @@ class M3uPlaylistPlayer(tk.Tk):
 
             for i, line in enumerate(lines):
                 if line.startswith("#EXTINF"):
-                    name = line[line.rfind(",")+1:].strip()
+                    name = line[line.rfind(",") + 1:].strip()
                     url = None
 
-                    for j in range(i+1, len(lines)):
+                    for j in range(i + 1, len(lines)):
                         url_line = lines[j].strip()
                         if url_line and not url_line.startswith("#"):
                             url = url_line
                             break
 
                     if url:
-                        list_number=len(self.playlist)+1
+                        list_number = len(self.playlist) + 1
                         self.tree.insert("", "end", values=(list_number, name, url))
                         self.playlist.append((name, url))
         except FileNotFoundError:
             simpledialog.messagebox.showerror("File Not Found", "The default playlist.m3u file was not found.")
-
-
-
 
     def play_channel(self, event):
         region = self.tree.identify_region(event.x, event.y)
@@ -211,7 +237,7 @@ class M3uPlaylistPlayer(tk.Tk):
         if region == "cell":
             item = self.tree.selection()[0]
             url = self.tree.item(item, "values")[2]
-            url =  '"' + url + '"'
+            url = '"' + url + '"'
             mpv_options = self.mpv_options_entry.get()
             bash_options = self.bash_options_entry.get()
             print("Playing channel:", url)
@@ -239,16 +265,15 @@ class M3uPlaylistPlayer(tk.Tk):
             elif os.system("mplayer -v") == 0:
                 os.system(f"mplayer {url} {mpv_options} &")
             else:
-                    print("No compatible video player found.")
-                    simpledialog.messagebox.showerror("Error", "No compatible video player found.")
-
+                print("No compatible video player found.")
+                simpledialog.messagebox.showerror("Error", "No compatible video player found.")
 
     # Function to add a channel
     def add_channel(self):
-        name = simpledialog.askstring("Add Channel", "Channel Name:"+rPadChars)
-        url = simpledialog.askstring("Add Channel", "Channel URL:"+rPadChars)
+        name = simpledialog.askstring("Add Channel", "Channel Name:" + rPadChars)
+        url = simpledialog.askstring("Add Channel", "Channel URL:" + rPadChars)
         if name and url:
-            self.list_number=len(self.tree.get_children())+1
+            self.list_number = len(self.tree.get_children()) + 1
             self.tree.insert("", "end", values=(self.list_number, name, url))
 
     # Function to delete a channel
@@ -258,7 +283,7 @@ class M3uPlaylistPlayer(tk.Tk):
             self.tree.delete(selection[0])
             # iterate over all items and update their list_number
             for i, item in enumerate(self.tree.get_children()):
-                self.tree.item(item, values=(i+1,) + tuple(self.tree.item(item)['values'][1:]))
+                self.tree.item(item, values=(i + 1,) + tuple(self.tree.item(item)['values'][1:]))
         else:
             simpledialog.messagebox.showerror("Error", "Select a channel to delete")
 
@@ -268,8 +293,10 @@ class M3uPlaylistPlayer(tk.Tk):
         if selection:
             item = selection[0]
             list_number, name, url = self.tree.item(item, "values")
-            name = simpledialog.askstring("Edit Channel", "Channel Name:"+rPadChars, initialvalue=self.tree.item(item, "values")[1])
-            url = simpledialog.askstring("Edit Channel", "Channel URL:"+rPadChars, initialvalue=self.tree.item(item, "values")[2])
+            name = simpledialog.askstring("Edit Channel", "Channel Name:" + rPadChars,
+                                          initialvalue=self.tree.item(item, "values")[1])
+            url = simpledialog.askstring("Edit Channel", "Channel URL:" + rPadChars,
+                                         initialvalue=self.tree.item(item, "values")[2])
             if name and url:
                 self.tree.item(item, values=(list_number, name, url))
         else:
@@ -303,8 +330,9 @@ class M3uPlaylistPlayer(tk.Tk):
         self.load_config()
         mpv_options = self.current_options["mpv_options"]
         bash_options = self.current_options["bash_options"]
-        self.mpv_options_entry.config(highlightthickness=2, highlightbackground="black")
-        self.bash_options_entry.config(highlightthickness=2, highlightbackground="black")
+        self.mpv_options_entry.config(highlightthickness=1, highlightbackground="black")
+        self.bash_options_entry.config(highlightthickness=1, highlightbackground="black")
+
         if not self.override_options.get():
             selection = self.tree.focus()
             if selection:
@@ -312,8 +340,8 @@ class M3uPlaylistPlayer(tk.Tk):
                 if url in self.current_options:
                     mpv_options = self.current_options[url].get("mpv_options", "")
                     bash_options = self.current_options[url].get("bash_options", "")
-                    self.mpv_options_entry.config(highlightthickness=2, highlightbackground="red")
-                    self.bash_options_entry.config(highlightthickness=2, highlightbackground="red")
+                    self.mpv_options_entry.config(highlightthickness=1, highlightbackground="red")
+                    self.bash_options_entry.config(highlightthickness=1, highlightbackground="red")
                 self.mpv_options_entry.delete(0, tk.END)
                 self.mpv_options_entry.insert(0, mpv_options)
                 self.bash_options_entry.delete(0, tk.END)
@@ -334,16 +362,16 @@ class M3uPlaylistPlayer(tk.Tk):
     def save_options(self):
         mpv_options = self.mpv_options_entry.get()
         bash_options = self.bash_options_entry.get()
-        self.mpv_options_entry.config(highlightthickness=2, highlightbackground="black")
-        self.bash_options_entry.config(highlightthickness=2, highlightbackground="black")
+        self.mpv_options_entry.config(highlightthickness=1, highlightbackground="black")
+        self.bash_options_entry.config(highlightthickness=1, highlightbackground="black")
         if self.override_options.get():
             self.current_options["mpv_options"] = mpv_options
             self.current_options["bash_options"] = bash_options
         else:
             selection = self.tree.focus()
             if selection:
-                self.mpv_options_entry.config(highlightthickness=2, highlightbackground="red")
-                self.bash_options_entry.config(highlightthickness=2, highlightbackground="red")
+                self.mpv_options_entry.config(highlightthickness=1, highlightbackground="red")
+                self.bash_options_entry.config(highlightthickness=1, highlightbackground="red")
                 url = self.tree.item(selection, "values")[2]
                 self.current_options[url] = {}
                 self.current_options[url]["mpv_options"] = mpv_options
@@ -354,38 +382,43 @@ class M3uPlaylistPlayer(tk.Tk):
         with open(config_file, "w") as file:
             json.dump(self.current_options, file)
 
-    #Change override
+    # Change override
     def change_override(self):
-        mpv_options = self.mpv_options_entry.get()
-        bash_options = self.bash_options_entry.get()
-        self.mpv_options_entry.config(highlightthickness=2, highlightbackground="black")
-        self.bash_options_entry.config(highlightthickness=2, highlightbackground="black")       
+        self.load_config()
+        self.mpv_options_entry.config(highlightthickness=1, highlightbackground="black")
+        self.bash_options_entry.config(highlightthickness=1, highlightbackground="black")
         if self.override_options.get():
-            self.mpv_options_entry.config(fg=self.mpv_bg, bg=self.mpv_fg)
-            self.bash_options_entry.config(fg=self.bash_bg, bg=self.bash_fg)
-            self.current_options["mpv_options"] = mpv_options
-            self.current_options["bash_options"] = bash_options
+            self.mpv_options_entry.config(fg=self.mpv_bg, bg=self.mpv_fg, insertbackground=self.mpv_bg)
+            self.bash_options_entry.config(fg=self.bash_bg, bg=self.bash_fg, insertbackground=self.bash_bg)
+            mpv_options = self.current_options["mpv_options"]
+            bash_options = self.current_options["bash_options"]
         else:
-            self.mpv_options_entry.config(fg=self.mpv_fg, bg=self.mpv_bg)
-            self.bash_options_entry.config(fg=self.bash_fg, bg=self.bash_bg)
+            self.mpv_options_entry.config(fg=self.mpv_fg, bg=self.mpv_bg, insertbackground=self.mpv_fg)
+            self.bash_options_entry.config(fg=self.bash_fg, bg=self.bash_bg, insertbackground=self.bash_fg)
             selection = self.tree.focus()
             if selection:
                 url = self.tree.item(selection, "values")[2]
                 if url in self.current_options:
-                    self.mpv_options_entry.config(highlightthickness=2, highlightbackground="red")
-                    self.bash_options_entry.config(highlightthickness=2, highlightbackground="red")
+                    self.mpv_options_entry.config(highlightthickness=1, highlightbackground="red")
+                    self.bash_options_entry.config(highlightthickness=1, highlightbackground="red")
                     mpv_options = self.current_options[url].get("mpv_options", "")
                     bash_options = self.current_options[url].get("bash_options", "")
-                self.mpv_options_entry.delete(0, tk.END)
-                self.mpv_options_entry.insert(0, mpv_options)
-                self.bash_options_entry.delete(0, tk.END)
-                self.bash_options_entry.insert(0, bash_options)
+        self.mpv_options_entry.delete(0, tk.END)
+        self.mpv_options_entry.insert(0, mpv_options)
+        self.bash_options_entry.delete(0, tk.END)
+        self.bash_options_entry.insert(0, bash_options)
         self.save_config()
 
-
     # Function About
-    def show_about_window(self):
-        simpledialog.messagebox.showinfo("About"+rPadChars, "playlist4whisper\n\nPlaylist for livestream_video.sh, a simple GUI using Python and tkinter library. It plays online videos and transcribe livestreams by feeding ffmpeg output to whisper.cpp, based on livestream.sh from whisper.cpp.\n\nAuthor: Antonio R.\nVersion: 1.22\nLicense: MIT")
+    @staticmethod
+    def show_about_window():
+        simpledialog.messagebox.showinfo("About",
+                                         "playlist4whisper\n"
+                                         "Playlist for livestream_video.sh, "
+                                         "it plays online videos and transcribes them. "
+                                         "A simple GUI using Python and Tkinter library. "
+                                         "Based on whisper.cpp.\n\n"
+                                         "Author: Antonio R.\nVersion: 1.24\nLicense: MIT")
 
 
 if __name__ == "__main__":
