bb16f019e5f7536f94f48edceb21d8d9aad50e26
Antonio R <33932744+antor44@users.noreply.github.com>
2023-05-04 15:34:15 +0200

    playlist4whisper.py v. 1.30

--- playlist4whisper.py
+++ playlist4whisper.py
@@ -1,11 +1,11 @@
 """
 
-"playlist4whisper" is an application that displays a playlist for livestream_video.sh, a simple GUI using Python and
-the tkinter library. It plays online videos and transcribes livestreams by feeding the output of ffmpeg to
-whisper.cpp, based on livestream.sh from whisper.cpp.
+"Playlist4Whisper" is an application that displays a playlist for "livestream_video.sh". It plays an online video
+ and uses AI technology to transcribe the audio into text. It supports multi-instance and multi-user execution,
+ and allows for changing options per channel and global options.
 
 
-Author: Antonio R. Version: 1.28 License: MIT
+Author: Antonio R. Version: 1.30 License: MIT
 
 
 
@@ -13,14 +13,20 @@ Usage:
 
 python playlist4whisper.py
 
-The program will load default playlist.m3u, and will store options in config.json
+-Support for multi-instance and multi-user execution
+-Support for IPTV, YouTube and Twitch
+
+The program will load the default playlists playlist_iptv.m3u, playlist_youtube.m3u, and playlist_twitch.m3u,
+ and will store options in config_xxx.json.
 
 
-This program depends on other Linux programs and their libraries, such as whisper.cpp and mpv. The script
-livestream_video.sh should be in the same directory as the compiled executable of whisper.cpp, which should have the
-default name "main". Additionally, it is necessary to download the Whisper model file from OpenAI and place it in the
-"models" directory with the correct format and name, as specified in the Whisper.cpp repository. This can be done
-using terminal commands like one of the following examples:
+
+To ensure proper functioning of this GUI, all whisper.cpp files (from the official releases),
+ as well as the script livestream_video.sh, should be copied to the same location as playlist4whisper.py.
+ The main executable of whisper.cpp, which is the primary example, should be in the same directory with
+ the default executable name 'main'. Additionally, the Whisper model file from OpenAI should be placed
+ in the "models" subdirectory with the correct format and name, as specified in the Whisper.cpp repository.
+ This can be done using terminal commands such as the following examples:
 
 make tiny.en
 
@@ -30,6 +36,8 @@ make small
 playlist4whisper.py depends on (smplayer, mpv or mplayer) video player and (gnome-terminal or konsole or
 xfce4-terminal).
 
+For YouTube yt-dlp is required (https://github.com/yt-dlp/yt-dlp)
+For Twitch streamlink is required (https://streamlink.github.io)
 
 Options for script:
 
@@ -93,19 +101,21 @@ import re
 import json
 import urllib.parse
 import tkinter as tk
-from tkinter import filedialog, ttk, simpledialog
+from tkinter import Tk, ttk, filedialog, simpledialog, PhotoImage
+
 
 # Default options
-default_mpv_options = "-geometry 1100"
-default_bash_options = "4 base auto"
-bash_script = "./livestream_video.sh"
-config_file = "config.json"
 rPadChars = 100 * " "
 
 
-class M3uPlaylistPlayer(tk.Tk):
-    def __init__(self):
-        super().__init__()
+class M3uPlaylistPlayer(tk.Frame):
+    def __init__(self, parent, spec, bash):
+        super().__init__(parent)
+        self.spec = spec
+        self.bash_script = bash
+        self.default_mpv_options = "-geometry 1100"
+        self.default_bash_options = "4 base auto"
+        self.playlist = []
 
         self.load_button = None
         self.load_label = None
@@ -122,10 +132,7 @@ class M3uPlaylistPlayer(tk.Tk):
         self.mpv_options_label = None
         self.options_frame = None
         self.tree = None
-        self.title("playlist4whisper")
-        self.geometry("800x800")
 
-        self.playlist = []
         self.current_options = {}
         self.list_number = 0
         self.override_options = tk.BooleanVar()
@@ -208,7 +215,8 @@ class M3uPlaylistPlayer(tk.Tk):
         self.load_button = tk.Button(self, text="About", command=self.show_about_window)
         self.load_button.pack(side=tk.LEFT)
 
-    def populate_playlist(self, filename="playlist.m3u"):
+    def populate_playlist(self):
+        filename = f'playlist_{self.spec}.m3u'
         try:
             with open(filename, "r", encoding='utf-8') as file:
                 lines = file.readlines()
@@ -229,7 +237,7 @@ class M3uPlaylistPlayer(tk.Tk):
                         self.tree.insert("", "end", values=(list_number, name, url))
                         self.playlist.append((name, url))
         except FileNotFoundError:
-            simpledialog.messagebox.showerror("File Not Found", "The default playlist.m3u file was not found.")
+            simpledialog.messagebox.showerror("File Not Found", f"The default playlist_{self.spec}.m3u file was not found.")
 
     def play_channel(self, event):
         region = self.tree.identify_region(event.x, event.y)
@@ -243,13 +251,13 @@ class M3uPlaylistPlayer(tk.Tk):
             print("Playing channel:", url)
 
             # Try launching gnome-terminal, konsole or xfce4-terminal
-            if os.path.exists(bash_script):
+            if os.path.exists(self.bash_script):
                 if os.system("gnome-terminal --version") == 0:
-                    os.system(f"gnome-terminal --tab -- /bin/bash -c '{bash_script} {url} {bash_options}; exec /bin/bash -i' &")
+                    os.system(f"gnome-terminal --tab -- /bin/bash -c '{self.bash_script} {url} {bash_options}; exec /bin/bash -i' &")
                 elif os.system("konsole --version") == 0:
-                    os.system(f"konsole --noclose -e '{bash_script} {url} {bash_options}' &")
+                    os.system(f"konsole --noclose -e '{self.bash_script} {url} {bash_options}' &")
                 elif os.system("xfce4-terminal --version") == 0:
-                    os.system(f"xfce4-terminal --hold -e '{bash_script} {url} {bash_options}' &")
+                    os.system(f"xfce4-terminal --hold -e '{self.bash_script} {url} {bash_options}' &")
                 else:
                     print("No compatible terminal found.")
                     simpledialog.messagebox.showerror("Error", "No compatible terminal found.")
@@ -351,8 +359,9 @@ class M3uPlaylistPlayer(tk.Tk):
                 self.bash_options_entry.insert(0, bash_options)
 
     def load_config(self):
-        self.current_options["mpv_options"] = default_mpv_options
-        self.current_options["bash_options"] = default_bash_options
+        config_file = f'config_{self.spec}.json'
+        self.current_options["mpv_options"] = self.default_mpv_options
+        self.current_options["bash_options"] = self.default_bash_options
         if os.path.exists(config_file):
             with open(config_file, "r") as file:
                 self.current_options = json.load(file)
@@ -382,6 +391,7 @@ class M3uPlaylistPlayer(tk.Tk):
         self.save_config()
 
     def save_config(self):
+        config_file = f'config_{self.spec}.json'
         with open(config_file, "w") as file:
             json.dump(self.current_options, file)
 
@@ -421,9 +431,56 @@ class M3uPlaylistPlayer(tk.Tk):
                                          "it plays online videos and transcribes them. "
                                          "A simple GUI using Python and Tkinter library. "
                                          "Based on whisper.cpp.\n\n"
-                                         "Author: Antonio R.\nVersion: 1.28\nLicense: MIT")
+                                         "Author: Antonio R.\nVersion: 1.30\nLicense: MIT")
+
+
+class MainApplication():
+    main_window =tk.Tk()
+    main_window.title("playlist4whisper")
+    main_window.geometry("800x800")
+
+    icon = "iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABGdBTUEAALGPC/xhBQAAAYRpQ0NQSUNDIHByb2ZpbGUAACiRfZE9SMNAHMVfv1C04mAHEYcM1cmCaBHdtApFqBBqhVYdTC79giYNSYqLo+BacPBjserg4qyrg6sgCH6AuLo4KbpIif9LCi1iPDjux7t7j7t3gL9RYaoZHAdUzTLSyYSQza0KXa8IIYhexDEjMVOfE8UUPMfXPXx8vYvxLO9zf44+JW8ywCcQzzLdsIg3iKc2LZ3zPnGElSSF+Jx4zKALEj9yXXb5jXPRYT/PjBiZ9DxxhFgodrDcwaxkqMRx4qiiapTvz7qscN7irFZqrHVP/sJwXltZ5jrNYSSxiCWIECCjhjIqsBCjVSPFRJr2Ex7+IccvkksmVxmMHAuoQoXk+MH/4He3ZmFywk0KJ4DQi21/jABdu0Czbtvfx7bdPAECz8CV1vZXG8D0J+n1thY9Avq3gYvrtibvAZc7wOCTLhmSIwVo+gsF4P2MvikHDNwCPWtub619nD4AGeoqdQMcHAKjRcpe93h3d2dv/55p9fcDs4VywQjulqoAAAAGYktHRAAAAAAAAPlDu38AAAAJcEhZcwAADdcAAA3XAUIom3gAAAAHdElNRQfnBQQLNjFUgOcTAAAD0klEQVR42u2b229MURTGf0ZbLYkqo3VphXqgIqkmXlwiLkGJS73wQhBFIuJFhEcSiVtCSjzwD/DWJsgglAckDU2lCYKKS2ijcal2OqaqUw9nT3Ls7nNmembmzJ4zXclOMz1r7z3fd9Y6a+111kCWy6gE5hZphqUbGEgVAaVADbARmAeUALmaETAAdAJtwC2gHniT6KIlwGXgDzCYYSMCXAPKnYJfDfzIQODyCAM7h+sCu4GrQI7F9UGgSzMXGA+Mtrl+HDgRz0KrgH4Fk6+AQ8BsDf0fAb4MqAWaLKxhV6xF/MB3aVI/cNjGGnSNbjuAoMIdKuwmXpAm/BVP/kyVhUCPhKnBSrlYMGRWPuqBXGerwhWqVIp7JKU2IM8jCd9DCdtJlVKDpHTMQxnvNgnbc5XSS0mp0kMEFErYQiqlLkmp0MFGU4FFmpIgJ3VFquTGPJzIAjH3EbBcMwLeSfjKAXwp2mwJ8EBTIv4TX4rXjxJxW1fX8Lm0z1rgibCIFdlIgNkiGoE7wOJsICBi8f81wGMdiEg1Aa3AUuBmDCK0cI1khsHoaJHM/0aM4kUqiVCGQTcJSDcR2hCQLiK0I8BtIrQlICrLRIi0I+JuAlFDewLMRDTFQURlMghwOxGKJX5gJTDXRqcTuE8SXnroZAHFwFmGFjHN4wtGVTrfSy7gx6jX/7IB/hWjNlngpYegm8C1IiAdwLUgIJ3A00rAWxcebloTMJjGOx4XAel439cOnMF4+xxOd+LhJgGdwHngIvBbl8wrJ9vuuJsEaHnH7QiISGcDn01NLxOBy+eeIR1lHdJTssTBJuNcCGdOJSThK5BZ6ZAmVDjYpFdHPwdmSuH2Z9Q6zQQ0S5O24B2pkT43q5Q2SCbSA0zxAPh84KOE7aCVYrukWE9i7bQ6iNz3FMJ4ja+UfYqU9RL2/Xc6yxEFnlOxwqKqHncPo0c4U6QMuK7A8R6p8UNl3tOAp+KvHDcbgYA4WIRsvkALRr9hvJKLUR7PSdDXZ2C0+FYrwnFQ7NEaz2LzBVtO+3Or4/zSecDeBPeKZ3zDQaPGZIzycyoIyAP2Ax9IfbP0M4z2XseyXrhEMggYAxwAPrkA/DWwnRil/+GEuFkYbbNzMH5AMdZ0rQqYZPq8DqMtxuyftaIAUmqxfh/Gq/KIwxsVFtls9AcTL9x86gYsLCBPhNbPNneqD7hiQ0xGiEzApjiAhwXw6V7ItQMKcFbAe8Vx2QtptiUBqhEEzjk8Zmc0AUGgzi7/9ioB3SLv9pMFEpCO0XVeNXU7ArqB08BEslA2AxMYkREZkUyTf3yN/Z4aqslDAAAAAElFTkSuQmCC"
+
+    main_window.iconphoto(True, PhotoImage(data=icon))
+
+    tab_control = ttk.Notebook(main_window)
+
+    tab1 = ttk.Frame(tab_control)
+    tab2 = ttk.Frame(tab_control)
+    tab3 = ttk.Frame(tab_control)
+
+    tab_control.add(tab1, text="IPTV", compound="left")
+    tab_control.add(tab2, text="YouTube", compound="left")
+    tab_control.add(tab3, text="Twitch", compound="left")
+
+    canvas1 = tk.Canvas(tab1, width=25, height=80, bg='black', highlightthickness=0)
+    canvas1.pack(side=tk.LEFT, fill=tk.Y)
+    canvas1.create_text(15, 40, text='IPTV', angle=90, fill='white', anchor='center')
+
+    canvas2 = tk.Canvas(tab2, width=25, height=80, bg='black', highlightthickness=0)
+    canvas2.pack(side=tk.LEFT, fill=tk.Y)
+    canvas2.create_text(15, 40, text='YouTube', angle=90, fill='white', anchor='center')
+
+    canvas3 = tk.Canvas(tab3, width=25, height=80, bg='black', highlightthickness=0)
+    canvas3.pack(side=tk.LEFT, fill=tk.Y)
+    canvas3.create_text(15, 40, text='Twitch', angle=90, fill='white', anchor='center')
+
+    spec1 = "iptv"
+    spec2 = "youtube"
+    spec3 = "twitch"
+
+    tab_control.pack(expand=True, fill=tk.BOTH, side=tk.LEFT)
+
+    playlist_player1 = M3uPlaylistPlayer(tab1, spec1, "./livestream_video.sh")
+    playlist_player1.pack(fill=tk.BOTH, expand=True)
+
+    playlist_player2 = M3uPlaylistPlayer(tab2, spec2, "./livestream_video.sh")
+    playlist_player2.pack(fill=tk.BOTH, expand=True)
+
+    playlist_player3 = M3uPlaylistPlayer(tab3, spec3, "./livestream_video.sh")
+    playlist_player3.pack(fill=tk.BOTH, expand=True)
 
 
 if __name__ == "__main__":
-    app = M3uPlaylistPlayer()
-    app.mainloop()
+    app = MainApplication()
+    app.main_window.mainloop()
